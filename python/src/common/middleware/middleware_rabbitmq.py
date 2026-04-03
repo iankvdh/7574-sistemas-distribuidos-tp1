a06cb4c35d0afcfaa7ed53064c4fe8d498dd0166
iankvdh <ivon@fi.uba.ar>
2026-04-03 14:42:54 -0300

    feat: iniciamos clases con su respectivo close() para pasar test_init_and_close

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,14 +1,62 @@
 import pika
 import random
 import string
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from .middleware import MessageMiddlewareCloseError, MessageMiddlewareQueue, MessageMiddlewareExchange
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self._channel = self._connection.channel()
+        self._queue_name = queue_name
+        self._channel.queue_declare(queue=self._queue_name, durable=True)
+
+    def close(self):
+        try:
+            if self._channel and self._channel.is_open:
+                self._channel.close()
+            if self._connection and self._connection.is_open:
+                self._connection.close()
+        except Exception as e:
+            raise MessageMiddlewareCloseError(f"Error al cerrar conexión: {e}")
+        
+    def send(self, message):
+        pass
+
+    def start_consuming(self, on_message_callback):
+        pass
+
+    def stop_consuming(self):
         pass
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self._channel = self._connection.channel()
+        self._exchange_name = exchange_name
+        self._routing_keys = routing_keys
+        self._channel.exchange_declare(
+            exchange=self._exchange_name, 
+            exchange_type='direct', 
+            durable=True
+        )
+
+
+    def close(self):
+        try:
+            if self._channel and self._channel.is_open:
+                self._channel.close()
+            if self._connection and self._connection.is_open:
+                self._connection.close()
+        except Exception as e:
+            raise MessageMiddlewareCloseError(f"Error al cerrar conexión: {e}")
+        
+    def send(self, message):
+        pass
+
+    def start_consuming(self, on_message_callback):
+        pass
+
+    def stop_consuming(self):
         pass
