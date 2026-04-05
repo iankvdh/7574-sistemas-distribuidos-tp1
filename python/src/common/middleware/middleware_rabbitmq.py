3825a860d1bd587d75c2f266e20021eb10e175c9
iankvdh <ivon@fi.uba.ar>
2026-04-05 18:09:36 -0300

    feat (exchange): se completaron las implementaciones de MessageMiddlewareExchangeRabbitMQ para que pase los "test_exchange". Además se agregó una verificación extra en los callback (if ch.is_open: )

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -51,10 +51,12 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         
         def callback(ch, method, properties, body):
             def ack():
-                ch.basic_ack(delivery_tag=method.delivery_tag)
+                if ch.is_open:    
+                    ch.basic_ack(delivery_tag=method.delivery_tag)
             
             def nack():
-                ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+                if ch.is_open:
+                    ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
 
             try:
                 on_message_callback(body, ack, nack)
@@ -92,6 +94,8 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
         self._exchange_name = exchange_name
         self._routing_keys = routing_keys
+        self._queue_name = None  # Solo se usará si somos consumidores
+
         try:
             self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
             self._channel = self._connection.channel()
@@ -100,6 +104,15 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
                 exchange_type='direct', 
                 durable=True
             )
+
+            # Lógica de Binding para Consumidores
+            if routing_keys:
+                result = self._channel.queue_declare(queue='', exclusive=True)
+                self._queue_name = result.method.queue
+
+                for key in routing_keys:
+                    self._channel.queue_bind(exchange=self._exchange_name, queue=self._queue_name, routing_key=key)
+
         except (pika.exceptions.AMQPConnectionError, pika.exceptions.StreamLostError):
             raise MessageMiddlewareDisconnectedError(f"No se pudo conectar a RabbitMQ en {host}")
         except Exception as e:
@@ -116,10 +129,60 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
             raise MessageMiddlewareCloseError(f"Error al cerrar conexión: {e}")
         
     def send(self, message):
-        pass
+        try:
+            routing_key = self._routing_keys[0] if self._routing_keys else ''
+            self._channel.basic_publish(
+                exchange=self._exchange_name,
+                routing_key=routing_key,
+                body=message,
+                properties=pika.BasicProperties(
+                    delivery_mode=2, # 2 es persistent, lo que hace que el mensaje se guarde en disco y no se pierda si RabbitMQ se reinicia
+                )
+            )
+        except (pika.exceptions.AMQPConnectionError, pika.exceptions.StreamLostError):
+            raise MessageMiddlewareDisconnectedError("Conexión perdida con RabbitMQ")
+        except pika.exceptions.AMQPError as e:
+            raise MessageMiddlewareMessageError(f"Error interno al enviar mensaje: {e}")
+
 
     def start_consuming(self, on_message_callback):
-        pass
+        self._channel.basic_qos(prefetch_count=1) # Asegura que solo se entregue un mensaje a la vez al consumidor para que si llegara a haber más consumidores la carga se reparta entre ellos.
+
+        
+        def callback(ch, method, properties, body):
+            def ack():
+                if ch.is_open:
+                    ch.basic_ack(delivery_tag=method.delivery_tag)
+            
+            def nack():
+                if ch.is_open:
+                    ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+
+            try:
+                on_message_callback(body, ack, nack)
+            except Exception as e:
+                if ch.is_open:
+                    nack()
+                print(f"Error en lógica de usuario (Exchange): {e}")
+
+
+        try:
+            self._channel.basic_consume(
+                queue=self._queue_name,
+                on_message_callback=callback
+            )
+
+            self._channel.start_consuming()
+            
+        except (pika.exceptions.AMQPConnectionError, pika.exceptions.StreamLostError):
+            raise MessageMiddlewareDisconnectedError("Conexión perdida durante el consumo")
+        except Exception as e:
+            raise MessageMiddlewareMessageError(f"Error interno en el consumidor: {e}")
+
 
     def stop_consuming(self):
-        pass
+            try:
+                if self._channel and self._channel.is_open:
+                    self._channel.stop_consuming()
+            except pika.exceptions.AMQPError:
+                raise MessageMiddlewareDisconnectedError("Error al detener el consumo")
